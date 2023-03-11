395c80f0a6a7c6129c43f556c112911dd5f8af3c
Can Kılıç <can.kilic@inavitas.com>
2023-03-11 19:13:31 +0300

    www remove

--- src/pages/PaymentPage/Payment.jsx
+++ src/pages/PaymentPage/Payment.jsx
@@ -26,7 +26,7 @@ function Payment() {
   const sendPaymentStatus = (deviceId, drawerId) => {
     let data = { deviceId: "4c:eb:d6:7c:11:74", drawerId: "2" };
 
-    fetch("https://www.vendugo.com/purchase", {
+    fetch("https://vendugo.com/purchase", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
